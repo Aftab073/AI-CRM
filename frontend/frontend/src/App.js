3cd9d6303a48531aa16ce4831b1a19ed5a19f017
Aftab073 <tamboliaftab84@gmail.com>
2025-06-10 11:53:36 +0530

    Add working backend

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,11 @@
+import LogInteractionScreen from './LogInteractionScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <LogInteractionScreen />
+    </div>
+  );
+}
+
+export default App;
